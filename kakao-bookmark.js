4341c428a1bfc17fc6931b3eef700b1a2a8a4ac5
musicofbugs <133899489+musicofbugs@users.noreply.github.com>
2024-11-26 21:47:11 +0900

    Update kakao-bookmark.js

--- kakao-bookmark.js
+++ kakao-bookmark.js
@@ -1,23 +1,27 @@
 javascript:(function() {
-
+    
     var divs = document.querySelectorAll('.DC2CN');
 
     if (divs.length > 0) {
-
-        var contentHtml = divs[0].outerHTML;
-
-
+    
         var titleElement = document.querySelector(".toon-title");
-        var fileName = titleElement ? titleElement.getAttribute("title") : "DC2CN_content";
-        fileName = fileName.replace(/<|>|\/|:|"|%27|\?|\\|\*|&|#|%|\s/g, "_").slice(0, 50);
-        fileName += ".html";
+        var baseFileName = titleElement ? titleElement.getAttribute("title") : "DC2CN_content";
+        baseFileName = baseFileName.replace(/<|>|\/|:|"|%27|\?|\\|\*|&|#|%|\s/g, "_").slice(0, 50);
 
+    
+        divs.forEach(function(div, index) {
+            var contentHtml = div.outerHTML;
 
-        var blob = new Blob([contentHtml], { type: "text/html" });
-        var a = document.createElement("a");
-        a.href = URL.createObjectURL(blob);
-        a.download = fileName;
-        a.click();
+    
+            var fileName = baseFileName + "_part_" + (index + 1) + ".html";
+
+    
+            var blob = new Blob([contentHtml], { type: "text/html" });
+            var a = document.createElement("a");
+            a.href = URL.createObjectURL(blob);
+            a.download = fileName;
+            a.click();
+        });
     } else {
         alert("DC2CN 클래스를 가진 div 요소를 찾을 수 없습니다.");
     }
